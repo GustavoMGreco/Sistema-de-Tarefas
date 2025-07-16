398669d1ac7e94c9a7e2a466b8e2ee24ffa421d8
Gustavo M Greco <gustavomonteirogreco@gmail.com>
2025-07-16 01:51:50 -0300

    Adição do config.py, criação do database e da rota 'banco-teste' para teste de conexão

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+import config
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def inicio():
-    return '<h1>Olá, Flask!<h1>'
+app.config.from_object('config')
+
+mysql = MySQL(app)
+
+@app.route('/banco-teste')
+def teste():
+    try:
+        cursor = mysql.connection.cursor()
+        
+        cursor.execute('SELECT NOW();')
+        resultado = cursor.fetchone()
+
+        cursor.close()
+
+        return f'Conexão bem-sucedida! Horário do servidor MySQL:{resultado[0]}'
+    except Exception as e:
+        return f'Erro ao conectar {e}'
+    
+
+if __name__ == ('__main__'):
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,4 @@
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'sunLOML17010605@'
+MYSQL_DB = 'SistemadeTarefas'
